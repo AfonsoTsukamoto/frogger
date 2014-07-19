90ade4492c06bc3f51bf6d03ffe162dd3d9bed25
Afonso Tsukamoto <atsukamoto@faber-ventures.com>
2014-07-19 17:31:06 +0100

    Initial Commit

---
+++ ProjCG/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ProjCG
+//
+//  Created by Afonso Tsukamoto on 7/19/14.
+//  Copyright (c) 2014 Afonso Tsukamoto. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
